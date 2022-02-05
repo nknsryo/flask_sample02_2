418b1fc191d54622f851166ed8b450b343bc62e3
kwdaaa <kwd.aaa.a@icloud.com>
2022-02-05 13:53:38 +0900

    nameを入れたら出るよ！

--- 01.py
+++ 01.py
@@ -7,9 +7,16 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world"
 
+
 @app.route("/login")
 def login():
     return "login!!"
 
+
+@app.route("/users/<name>")
+def hi(name):
+    return f"Hi.{name}!!"
+
+
 if __name__ == '__main__':
-    app.run(debug=True,port=5001)
+    app.run(debug=True, port=5001)
