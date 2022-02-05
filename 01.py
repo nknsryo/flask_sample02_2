d810d008b989f2fb7db0f8af4302f48f52d2f629
nknsryo <nkns.ryo@gmail.com>
2022-02-05 13:50:15 +0900

    ログイン表示。

--- 01.py
+++ 01.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world"
 
+@app.route("/login")
+def login():
+    return "login!!"
 
 if __name__ == '__main__':
     app.run(debug=True,port=5001)
